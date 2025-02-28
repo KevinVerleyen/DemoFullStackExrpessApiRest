315c9e179bc9439f6f03ba3d8e97dbe17451397a
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-02-28 11:48:57 +0100

    express APIREST

---
+++ models/livre.model.js
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  class Livre extends Model {
+    static associate(models) {}
+  }
+
+  Livre.init(
+    {
+      titre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      auteur: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      annee: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Livre",
+      tableName: "Livres",
+      timestamps: true,
+    }
+  );
+
+  return Livre;
+};
